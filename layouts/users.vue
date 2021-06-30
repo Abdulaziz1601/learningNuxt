9484986b5a2aaf6bb434adbe3f23dbf3169fe067
Abdulaziz <abdulazizfaactory@gmail.com>
2021-06-30 18:57:18 +0500

    end

--- layouts/users.vue
+++ layouts/users.vue
@@ -1,18 +1,18 @@
-<template>
-	<div>
-        <p>User</p>
-		<Header />
-		<Nuxt />
-	</div>
-</template>
+	<template>
+		<div>
+			<p>User</p>
+			<Header />
+			<Nuxt />
+		</div>
+	</template>
 
-<script>
-import Header from '@/components/Header'
+	<script>
+	import Header from '@/components/Header'
 
-export default {
-	components: {
-		Header: Header
-	},
-}
-</script>
+	export default {
+		components: {
+			Header: Header
+		},
+	}
+	</script>
 
